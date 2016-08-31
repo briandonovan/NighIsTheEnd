0e923e2c7926ebe8262a639be96789170c39ef51
Brian Donovan <brian@briandon.com>
2016-09-01 01:45:32 +0800

    Initial commit.

---
+++ NighIsTheEnd.rb
@@ -0,0 +1,25 @@
+
+def tally_chars_in_msg(str_msg)
+  hsh_tally = {}
+  str_msg.each_char{|c|
+    if hsh_tally.has_key?(c)
+      hsh_tally[c] += 1
+    else
+      hsh_tally[c] = 1
+    end
+  }
+  return hsh_tally
+end
+
+puts "Type in the message you'd like to tally and press /ENTER/!"
+str_msg = gets.chomp
+if !str_msg.instance_of? String
+  abort("Somehow, you fed a non-string to this script.")
+end
+if str_msg.length === 0
+  abort("You didn't write anything.")
+end
+
+hsh_tally = tally_chars_in_msg(str_msg)
+puts "That message included #{hsh_tally.keys.length} different characters."
+exit
