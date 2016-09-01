ae4ddac3da3a3447516fae2b5217635b19e2ba47
Brian Donovan <brian@briandon.com>
2016-09-01 21:37:50 +0800

    Fiddling with colors some more.

--- NighIsTheEnd.rb
+++ NighIsTheEnd.rb
@@ -20,7 +20,7 @@ end
 def rtn_str_char_occur_list_printable(arr_hsh_tally_sorted)
   str_printable_list = ''
   for arr_char_tally in arr_hsh_tally_sorted
-    str_printable_list += $str_nlnsp + "#{arr_char_tally[0].to_s.cyan}: #{arr_char_tally[1].to_s.yellow}"
+    str_printable_list += $str_nlnsp + "#{arr_char_tally[0].to_s.cyan}: #{arr_char_tally[1].to_s.magenta}"
   end
   str_printable_list += "\n"
   return str_printable_list
@@ -45,15 +45,15 @@ arr_vals_hsh_tally = hsh_tally.values.sort.reverse
 fxnm_highest_occurrence = arr_vals_hsh_tally[0]
 fxnm_num_chars_highest_occur = arr_vals_hsh_tally.count(fxnm_highest_occurrence)
 
-puts $str_nlnsp + "That message included #{hsh_tally.keys.length.to_s.yellow} different characters."
+puts $str_nlnsp + "That message included #{hsh_tally.keys.length.to_s.magenta.underline} different characters."
 
 if fxnm_highest_occurrence == 1
   puts $str_nlnsp + "No character occurs more than once."
 elsif fxnm_num_chars_highest_occur == 1
-  puts $str_nlnsp + "The character #{arr_hsh_tally_sorted[0][0].cyan} was most common, present #{arr_hsh_tally_sorted[0][1].to_s.yellow} times."
+  puts $str_nlnsp + "The character #{arr_hsh_tally_sorted[0][0].cyan} was most common, present #{arr_hsh_tally_sorted[0][1].to_s.magenta.underline} times."
 else
   str_highest_occur_chars = arr_hsh_tally_sorted[0, fxnm_num_chars_highest_occur].map { |k| "#{k[0].to_s.cyan}" }.join(" ")
-  puts $str_nlnsp + "#{fxnm_num_chars_highest_occur.to_s} characters (#{str_highest_occur_chars}) were each present #{fxnm_highest_occurrence.to_s.yellow} times."
+  puts $str_nlnsp + "#{fxnm_num_chars_highest_occur.to_s} characters (#{str_highest_occur_chars}) were each present #{fxnm_highest_occurrence.to_s.magenta.underline} times."
 end
 
 str_printable_char_occur_list = rtn_str_char_occur_list_printable(arr_hsh_tally_sorted)
